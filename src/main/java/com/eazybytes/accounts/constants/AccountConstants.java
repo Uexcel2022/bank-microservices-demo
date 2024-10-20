00982e85297b07d477729cfd10a4e955a6d4bbc9
Uexcel2022 <excellenceudoka@gmail.com>
2024-10-21 00:31:23 +0100

    improvements

--- src/main/java/com/eazybytes/accounts/constants/AccountConstants.java
+++ src/main/java/com/eazybytes/accounts/constants/AccountConstants.java
@@ -11,15 +11,12 @@ public class AccountConstants {
     public static final String MESSAGE_201 = "Account details created successfully";
     public static final String STATUS_200 = "200";
     public static final String MESSAGE_200 = "Request processed successfully";
-    public static final String STATUS_500 = "500";
-    public static final String MESSAGE_500 = "An error occurred. Please try again or contact dev team";
     public static final String STATUS_417 = "417";
     public static final String MESSAGE_417_Update = "Update failed. Please try again or contact dev team";
     public static final String MESSAGE_417_Delete = "An error failed. Please try again or contact dev team";
     public static final  String  STATUS_200_desc = "HTTP Status OK";
     public static final  String  STATUS_201_desc = "HTTP Status Created";
     public static final  String  STATUS_404_desc = "Not Found";
-    public static final  String  STATUS_400_desc = "Bad Request";
     public static final  String  STATUS_500_desc = "Internal Server Error";
     public static final  String  STATUS_417_desc = "Exception Failed";
 }
